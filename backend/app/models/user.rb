193ad5379439f9159527c13ea2ca3909f32b9136
Nick Bucciarelli <nick.bucciarelli@gmail.com>
2014-08-23 12:07:03 -0400

    Added twitter login.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,6 +2,18 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  TEMP_EMAIL_PREFIX = 'change@me'
+  def self.create_with_omniauth(auth)
+    new_email = auth.try(:[], 'info').try(:[], 'email')
+    new_user = self.new(
+      provider: auth["provider"],
+      uid: auth["uid"],
+      name: auth["info"]["name"],
+      email: new_email ? new_email : "#{TEMP_EMAIL_PREFIX}-#{auth['uid']}-#{auth['provider']}.com",
+      password: Devise.friendly_token[0,20]
+    )
+    new_user.save!
+    new_user
+  end
 end
