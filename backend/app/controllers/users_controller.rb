873aaaef718a5c13dc04a499727631fe85f4bff7
Nick Bucciarelli <nick.bucciarelli@gmail.com>
2014-08-23 16:47:07 -0400

    Endpoint created.

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def create
+    @user = User.new user_params
+    @user.save!(validate: false)
+  end
+
+  private
+  def user_params
+    params.require(:login_info).permit(:email, :password)
+  end
+
 end
