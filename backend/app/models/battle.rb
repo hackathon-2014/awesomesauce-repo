b83f89a02b1107a2d3ff888b6163fe5c5f51f54e
Kevin Collins <kevincol54@gmail.com>
2014-08-23 14:50:51 -0400

    battle relationships

--- backend/app/models/battle.rb
+++ backend/app/models/battle.rb
@@ -1,4 +1,6 @@
 class Battle < ActiveRecord::Base
+  belongs_to :challenger, class_name: "User", foreign_key: :challenger_id
+  belongs_to :challengee, class_name: "User", foreign_key: :challengee_id
 
   include workflow
   workflow do 
