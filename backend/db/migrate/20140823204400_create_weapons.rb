a6b8e11c9fd485224461a3241c02f7dc9b9e4300
Kevin Collins <kevincol54@gmail.com>
2014-08-23 16:47:07 -0400

    db

--- backend/db/migrate/20140823204400_create_weapons.rb
+++ backend/db/migrate/20140823204400_create_weapons.rb
@@ -1,7 +1,8 @@
 class CreateWeapons < ActiveRecord::Migration
   def change
     create_table :weapons do |t|
-
+      t.integer :spell_id
+      t.integer :battle_id
       t.timestamps
     end
   end
